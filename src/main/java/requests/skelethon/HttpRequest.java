08e146b11095a70a789379a4287af6baef2e3deb
maria <mdobrinevskaya@gmail.com>
2026-09-10 18:58:26 +0300

    finished autotests senior

--- src/main/java/requests/skelethon/HttpRequest.java
+++ src/main/java/requests/skelethon/HttpRequest.java
@@ -4,7 +4,6 @@ import io.restassured.specification.RequestSpecification;
 import io.restassured.specification.ResponseSpecification;
 
 public class HttpRequest {
-    //почему эти поля protected а не private?
     protected RequestSpecification requestSpecification;
     protected Endpoint endpoint;
     protected ResponseSpecification responseSpecification;
